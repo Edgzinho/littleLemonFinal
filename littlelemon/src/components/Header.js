4e50734e886c55d329478933adab52b58a04e41e
Edgzinho <57346889+Edgzinho@users.noreply.github.com>
2024-12-28 15:43:35 +0000

    add

---
+++ littlelemon/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return(
+        <header>
+            <section>
+            <div>
+            <h2>Little Lemon</h2>
+            <h3>Chicago</h3>
+            <p>We are a family owned Mediterraneran restaurant, focused on traditional recipes servred with a modern twist.</p>
+            <Link to="/Booking"><button aria-label="On Click">Reserve Table</button></Link>
+            </div>
+
+            </section>
+        </header>
+    )
+}
+
+export default Header;
